c1fab59ff9f42e3cd14344dfb12ce3a0b30cb201
hasankoklu <54498970+hasankoklu@users.noreply.github.com>
2020-06-09 11:27:33 +0300

    meta

--- Assets/Scripts/PlayfabManager.cs
+++ Assets/Scripts/PlayfabManager.cs
@@ -1,26 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using PlayFab;
+using PlayFab.ClientModels;
 using UnityEngine;
-using UnityEngine.Networking;
 
 public class PlayfabManager : MonoBehaviour
 {
-    void Start()
+    public void Start()
     {
-
+        if (string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId))
+        {
+            /*
+            Please change the titleId below to your own titleId from PlayFab Game Manager.
+            If you have already set the value in the Editor Extensions, this can be skipped.
+            */
+            PlayFabSettings.staticSettings.TitleId = "42";
+        }
+        var request = new LoginWithGoogleAccountRequest {};
+        PlayFabClientAPI.LoginWithGoogleAccount(request, OnLoginSuccess, OnLoginFailure);
     }
 
-    public IEnumerator LinkGoogleAccount()
+    private void OnLoginSuccess(LoginResult result)
     {
-        string url = "https://titleId.playfabapi.com/Client/LinkGoogleAccount";
-
-        UnityWebRequest.Post(url, "");
-
-        yield return new WaitForEndOfFrame();
+        Debug.Log("Congratulations, you made your first successful API call!");
     }
 
-    void Update()
+    private void OnLoginFailure(PlayFabError error)
     {
-
+        Debug.LogWarning("Something went wrong with your first API call.  :(");
+        Debug.LogError("Here's some debug information:");
+        Debug.LogError(error.GenerateErrorReport());
     }
 }
