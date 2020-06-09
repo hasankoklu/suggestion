b66b115dd6e8d33276c9a7f00b5e8ff66f0a054e
hasankoklu <54498970+hasankoklu@users.noreply.github.com>
2020-06-09 10:29:32 +0300

    comit

---
+++ Assets/Scripts/PlayfabManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayfabManager : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    public IEnumerator LinkGoogleAccount()
+    {
+        string url = "https://titleId.playfabapi.com/Client/LinkGoogleAccount";
+
+        UnityWebRequest.Post(url, "");
+
+        yield return new WaitForEndOfFrame();
+    }
+
+    void Update()
+    {
+
+    }
+}
